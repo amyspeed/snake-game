a42cf0c1e029689205b3f3db7c3411c8b5678327
Amy Speed <amy.speed.henley@gmail.com>
2020-07-13 20:12:37 -0500

    sign in screen added

--- src/GameOver.js
+++ src/GameOver.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignIn from './SignIn';
 
 const GameOver = (props) => {
+    const [showSignIn, handleShowSignIn] = useState(false);
 
     return (
         <div className="overlay-container">
-            <h1>Game Over...</h1>
-            <p>Your space snake <b>{props.problem}!</b><br/>
-            <span>{props.length}</span> energy block{props.length === 1 ? '' : 's'} collected</p>
+            { !showSignIn ?
+                <div>
+                    <h1>Game Over...</h1>
+                    <p>Your space snake <b>{props.problem}!</b><br/>
+                    <span>{props.length}</span> energy block{props.length === 1 ? '' : 's'} collected</p>
             
-            <button className="new-game-button" onClick={(e) => props.startNewGame(e)}>New Game</button>
+                    <button className="new-game-button" onClick={(e) => props.startNewGame(e)}>New Game</button>
+                </div>
+            :
+                <SignIn />
+            }
+            { !props.signedIn ?
+            <p className="sign-in-link" onClick={() => handleShowSignIn(!showSignIn)}>{ showSignIn ? 'cancel' : 'Sign in to save score' }</p>
+            : null }
         </div>
     )
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignIn from './SignIn';
 
 const Welcome = (props) => {
+    const [showSignIn, handleShowSignIn] = useState(false);
 
+
+    console.log(showSignIn)
     return (
         <div className="overlay-container">
-            <h1>Welcome to Space Snake!</h1>
-            <p className="instructions">Your job is to collect as many 
-                energy blocks as you can without crashing or 
-                collapsing.
-            </p>
-            <p className="instructions">
-                Use your keyboard arrow keys or the provided buttons 
-                to affect your Space Snake's movements. Enjoy!
-            </p>
+            { !showSignIn ?
+                <div>
+                    <h1>Welcome to Space Snake!</h1>
+                    <p className="instructions">Your job is to collect as many 
+                        energy blocks as you can without crashing or 
+                        collapsing.
+                     </p>
+                    <p className="instructions">
+                        Use your keyboard arrow keys or the provided buttons 
+                        to affect your Space Snake's movements. Enjoy!
+                    </p>
             
-            <button className="new-game-button" onClick={(e) => props.startNewGame(e)}>Start Game</button>
+                    <button className="new-game-button" onClick={(e) => props.startNewGame(e)}>Start Game</button>
+                </div>
+            :
+                <SignIn />
+            }
+            <p className="sign-in-link" onClick={() => handleShowSignIn(!showSignIn)}>{ showSignIn ? 'instructions' : 'Sign In' }</p>
         </div>
     )
 }
